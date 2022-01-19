dadfcc6eb55ac6fd2accefe1eab9c9d313742de4
shihab <shumonshihab62@gmail.com>
2022-01-19 21:45:03 +0600

    designed the appointment modal

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -1,61 +1,71 @@
-import React, { useState } from 'react';
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
-import useAuth from '../../../hooks/useAuth';
-
+import React, { useState } from "react";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import useAuth from "../../../hooks/useAuth";
+import "./bookingModal.css";
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
-const BookingModal = ({ setBookingSuccess, openBooking, handleBookingClose, booking, date }) => {
+const BookingModal = ({
+  setBookingSuccess,
+  openBooking,
+  handleBookingClose,
+  booking,
+  date,
+}) => {
   const { name, time, price } = booking;
   const { user } = useAuth();
-  const initialInfo = { patientName: user.displayName, email: user.email, phone: "" }
+  const initialInfo = {
+    patientName: user.displayName,
+    email: user.email,
+    phone: "",
+  };
   const [bookingInfo, setBookingInfo] = useState(initialInfo);
-  const handleOnBlur = e => {
+  const handleOnBlur = (e) => {
     const field = e.target.name;
     const value = e.target.value;
     const newInfo = { ...bookingInfo };
     newInfo[field] = value;
-    setBookingInfo(newInfo)
-  }
-  const handleBookSubmit = e => {
+    setBookingInfo(newInfo);
+  };
+  const handleBookSubmit = (e) => {
     // collect data from input fields
     const appointment = {
       ...bookingInfo,
       time,
       price,
       serviceName: name,
-      date: date.toLocaleDateString()
-    }
+      date: date.toLocaleDateString(),
+    };
     fetch("https://aqueous-garden-06025.herokuapp.com/appointments", {
       method: "POST",
       headers: {
-        "content-type": "application/json"
+        "content-type": "application/json",
       },
-      body: JSON.stringify(appointment)
+      body: JSON.stringify(appointment),
     })
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         if (data.insertedId) {
           setBookingSuccess(true);
           handleBookingClose();
         }
-      })
+      });
     e.preventDefault();
-  }
+  };
   return (
     <Modal
       aria-labelledby="transition-modal-title"
@@ -67,11 +77,21 @@ const BookingModal = ({ setBookingSuccess, openBooking, handleBookingClose, book
       BackdropProps={{
         timeout: 1000,
       }}
-      sx={{ border: "none" }}
     >
       <Fade in={openBooking}>
         <Box sx={style}>
-          <Typography id="transition-modal-title" sx={{ textAlign: 'center', fontWeight: 500, my: 2, color: "var(--color)" }} variant="h5" component="h2">
+          <Typography
+            id="transition-modal-title"
+            sx={{
+              textAlign: "center",
+              fontWeight: 500,
+              my: 2,
+              color: "var(--color)",
+              fontFamily: "var(--montserrat-font)",
+            }}
+            variant="h5"
+            component="h2"
+          >
             {name}
           </Typography>
           <form onSubmit={handleBookSubmit} style={{ marginLeft: "20px" }}>
@@ -113,7 +133,9 @@ const BookingModal = ({ setBookingSuccess, openBooking, handleBookingClose, book
               id="outlined-size-small"
               size="small"
             />
-            <Button variant="contained" type="submit" className="btnStyle">Send</Button>
+            <Button variant="contained" type="submit" className="btnStyle">
+              Send
+            </Button>
           </form>
         </Box>
       </Fade>
